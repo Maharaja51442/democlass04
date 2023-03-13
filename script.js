b319cfcd7ac4f2aa1d8d136e398c77a4e5cb7d1e
Maharaja51442 <122883257+Maharaja51442@users.noreply.github.com>
2023-03-13 13:11:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function woo(){
+    var form1=document.getElementById("firstname").value;
+    var form2=document.getElementById("middlename").value;
+    var form3=document.getElementById("lastname").value;
+    var form4=document.getElementById("email").value;
+    var form5=document.getElementById("pass").value;
+    console.log(form1);
+    console.log(form2);
+    console.log(form3);
+    console.log(form4);
+    console.log(form5);
+}
